e4a0d8b652fa10fc790e1fed92029d38faa4c850
Till Wehowski <github@webfan.de>
2020-01-09 09:07:07 +0100

    Create MethodDiscoverableInterface.php

---
+++ src/MethodDiscoverableInterface.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace frdlweb\Api\Rpc;
+
+use stdClass;
+
+/**
+ * Contract for JSON-RPC 2.0 procedures.
+ */
+interface MethodDiscoverableInterface extends \UMA\JsonRpc\Procedure
+{
+  public function discover(DiscoverMethod $DiscoverMethod) : void;
+  public function getResultSpec(): ?\stdClass;
+  public function getSummary(): ?string;
+	public function getDescription(): ?string;
+	public function getLinks(): ?array;
+	public function getExamples(): ?array;
+	public function getParametersSpec(): ?array;
+}
